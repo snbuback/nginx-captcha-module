87d94e11011fdc01957bbefbdf9cebadabb59e7d
Silvano Buback <snbuback@gmail.com>
2012-01-28 00:41:19 -0200

    modificado funcao write_session_cookie para usar write_custom_header

--- src/ngx_captcha_utils.c
+++ src/ngx_captcha_utils.c
@@ -5,13 +5,12 @@
 #include "ngx_captcha_utils.h"
 
 const ngx_str_t SET_COOKIE_HEADER_TEMPLATE = ngx_string("%s=%s; Expires=-1; Path=/; HttpOnly");
-
+const ngx_str_t SET_COOKIE_HEADER = ngx_string("Set-Cookie");
 
 /**
  * Write a Set-Cookie header in response.
  */
-ngx_int_t write_session_cookie(ngx_http_request_t *r, ngx_str_t *cookie_name, ngx_str_t *cookie_value) {
-    ngx_table_elt_t  *set_cookie;
+ngx_int_t write_session_cookie(ngx_http_request_t *r, const ngx_str_t *cookie_name, const ngx_str_t *cookie_value) {
     
     // Validate arguments
     if (!(cookie_name && cookie_name->data && cookie_value && cookie_value->data)) {
@@ -19,29 +18,18 @@ ngx_int_t write_session_cookie(ngx_http_request_t *r, ngx_str_t *cookie_name, ng
     }
     
     // allocate memory for cookie header
-    ngx_str_t cookie_header = ngx_null_string;
-    cookie_header.data = ngx_palloc(r->pool, sizeof(u_char)*(SET_COOKIE_HEADER_TEMPLATE.len + cookie_name->len + cookie_value->len + 1));
+    ngx_str_t *cookie_header = ngx_palloc(r->pool, sizeof(ngx_str_t));
+    cookie_header->data = ngx_palloc(r->pool, sizeof(u_char)*(SET_COOKIE_HEADER_TEMPLATE.len + cookie_name->len + cookie_value->len + 1));
     
-    cookie_header.len = sprintf((char*) cookie_header.data, (char*) SET_COOKIE_HEADER_TEMPLATE.data, cookie_name->data, cookie_value->data);
+    cookie_header->len = sprintf((char*) cookie_header->data, (char*) SET_COOKIE_HEADER_TEMPLATE.data, cookie_name->data, cookie_value->data);
     
-    set_cookie = ngx_list_push(&r->headers_out.headers);
-    if (set_cookie == NULL) {
-        ngx_log_error(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
-                       "erro writing cookie '%s'='%s'", cookie_name->data, cookie_value->data);
-        return NGX_ERROR;
-    }
-
-    set_cookie->hash = 1;
-    ngx_str_set(&set_cookie->key, "Set-Cookie");
-    set_cookie->value = cookie_header;
-
-    return NGX_OK;
+    return write_custom_header(r, &SET_COOKIE_HEADER, cookie_header);
 }
 
 /**
  * Write a custom header in response
  */
-ngx_int_t write_custom_header(ngx_http_request_t *r, ngx_str_t *name, ngx_str_t *value) {
+ngx_int_t write_custom_header(ngx_http_request_t *r, const ngx_str_t *name, const ngx_str_t *value) {
     ngx_table_elt_t  *set_custom_header;
     
     // Validate arguments
@@ -57,8 +45,10 @@ ngx_int_t write_custom_header(ngx_http_request_t *r, ngx_str_t *name, ngx_str_t 
     }
 
     set_custom_header->hash = 1;
-    ngx_str_set(&set_custom_header->key, name->data);
-    ngx_str_set(&set_custom_header->value, value->data);
+    set_custom_header->key.len = name->len;
+    set_custom_header->key.data = name->data;
+    set_custom_header->value.len = value->len;
+    set_custom_header->value.data = value->data;
 
     return NGX_OK;
 }
--- src/ngx_captcha_utils.h
+++ src/ngx_captcha_utils.h
@@ -1,7 +1,8 @@
 #ifndef _NGX_CAPTCHA_UTILS_H_INCLUDED_
 #define _NGX_CAPTCHA_UTILS_H_INCLUDED_
 
-ngx_int_t write_session_cookie(ngx_http_request_t *r, ngx_str_t *cookie_name, ngx_str_t *cookie_value);
+ngx_int_t write_custom_header(ngx_http_request_t *r, const ngx_str_t *name, const ngx_str_t *value);
+ngx_int_t write_session_cookie(ngx_http_request_t *r, const ngx_str_t *cookie_name, const ngx_str_t *cookie_value);
     
 #endif
 
